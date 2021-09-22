94a2110a2c797125f218a21e579e51b5cd689764
Jordan Roberts <jxavier831@gmail.com>
2021-09-22 16:54:44 -0400

    Completed Literal Math Method Exercise

---
+++ script.js
@@ -0,0 +1,26 @@
+// LITERAL MATH METHOD
+
+// 1
+let warmHugs = "Hi, I'm Olaf and I like warm hugs";
+console.log(warmHugs.toUpperCase());
+console.log(warmHugs.replace("like", "love"));
+warmHugs = "Hi, I'm Olaf and I love warm hugs";
+console.log(warmHugs);
+
+// 2
+let beenImpaled = "Oh, look at that. I've been impaled."
+console.log(beenImpaled.slice(18, 36));
+
+// 3
+let dotDotDot = "...";
+let skullBones = `I don't have a skull${dotDotDot}or bones`;
+console.log(skullBones);
+
+// 4
+console.log(Math.PI);
+
+// 5
+let randomNumber = Math.random();
+randomNumber *=4;
+randomNumber = Math.floor(randomNumber);
+console.log(randomNumber);
